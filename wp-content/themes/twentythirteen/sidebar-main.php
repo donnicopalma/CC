7d45e43c832753fd5c1ca24647bbf44b60d745b8
skahler <stefan.kahler.v@gmail.com>
2014-03-12 15:32:32 -0300

    marquesina

--- wp-content/themes/twentythirteen/sidebar-main.php
+++ wp-content/themes/twentythirteen/sidebar-main.php
@@ -37,13 +37,14 @@ if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 					<div class="redes1">
 						<img class="redespng" src="wp-content/themes/twentythirteen/images/youtube.png"/>
 					</div>
+				<div id="carousel1">
+					<img alt="imagen 1" src="wp-content/themes/twentythirteen/images/facebook.png"/>
+					<img alt="imagen 2" src="wp-content/themes/twentythirteen/images/twitter.png"/>
+					<img alt="imagen 3" src="wp-content/themes/twentythirteen/images/google+.png"/>
+					<img alt="imagen 4" src="wp-content/themes/twentythirteen/images/youtube.png"/>
 				</div>
-				<div id="carrusel">
-					<img class="redespng" src="wp-content/themes/twentythirteen/images/facebook.png"/>
-					<img class="redespng" src="wp-content/themes/twentythirteen/images/twitter.png"/>
-					<img class="redespng" src="wp-content/themes/twentythirteen/images/google+.png"/>
-					<img class="redespng" src="wp-content/themes/twentythirteen/images/youtube.png"/>
 				</div>
+				
 			</div><!-- .widget-area -->
 			
 	</div><!-- #secondary -->
