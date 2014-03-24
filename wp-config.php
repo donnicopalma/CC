cfd235f468a4857ece9e7aa7d260b098b8112357
donnicopalma <nico.palma@gmail.com>
2014-03-24 18:10:52 -0300

    Página bien alineada

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'CITT_Clab');
+define('DB_NAME', 'citt_clab');
 
 /** MySQL database username */
 define('DB_USER', 'root');
--- wp-content/themes/twentythirteen/index.php
+++ wp-content/themes/twentythirteen/index.php
@@ -38,26 +38,28 @@ get_header(); ?>
             </div><!-- #DESTACADOS_cuadrados -->
         </div><!-- #second -->
 	</div>
-    <div id="contenedor">
-	<div class="noticias_index">
-        <div id="third" class="content-area">
-            <div id="content" class="site-content" role="main">
-            <?php if ( have_posts() ) : ?>
-                <?php /* The loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'content', get_post_format() ); ?>
-                <?php endwhile; ?>
-    
-                <?php twentythirteen_paging_nav(); ?>
-    
-            <?php else : ?>
-                <?php get_template_part( 'content', 'none' ); ?>
-            <?php endif; ?>
-    
-            </div><!-- #content -->
-        </div><!-- #third -->
-	</div>
+    <div class="medio">
+        <div id="contenedor">
+        <div class="noticias_index">
+            <div id="third" class="content-area">
+                <div id="content" class="site-content" role="main">
+                <?php if ( have_posts() ) : ?>
+                    <?php /* The loop */ ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'content', get_post_format() ); ?>
+                    <?php endwhile; ?>
+        
+                    <?php twentythirteen_paging_nav(); ?>
+        
+                <?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
+                <?php endif; ?>
+        
+                </div><!-- #content -->
+            </div><!-- #third -->
+        </div>
 		</div>
+      </div>  
 	<div class="noticias_index"><?php get_sidebar(); ?></div>
 
 <?php get_footer(); ?>
