75b44a2dab84d934861dc5e1fe66152b79cd3c43
skahler <stefan.kahler.v@gmail.com>
2014-04-24 20:29:13 -0300

    Pagina interior
    Trabajo en página interior

--- wp-content/themes/twentythirteen/content.php
+++ wp-content/themes/twentythirteen/content.php
@@ -13,7 +13,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="bgcontsuperior">
+
 			<h1 class="entry-title"><?php the_title(); ?></h1>
+
 			<div class="entry-thumbnail">
 				<?php the_post_thumbnail(); ?>
 			</div>
@@ -29,8 +31,8 @@
 			<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			<?php endif; // is_single() ?></h1>
 			
-				<?php twentythirteen_entry_meta(); ?>
-				<?php edit_post_link( __( 'Edit', 'twentythirteen' ),  '</span>' ); ?>
+				
+				<!-- <?php edit_post_link( __( 'Edit', 'twentythirteen' ),  '</span>' ); ?> -->
 			<!-- .entry-meta -->
 		<!-- .entry-header -->
 
@@ -43,15 +45,29 @@
 				<div class="contescrito">
 					<h2>DETALLES</h2>
 					<img alt="imagen 1" src="wp-content/themes/twentythirteen/images/lineadecorado.jpg"/>
+					
+					<p>Added by  
+					<?php the_author_posts_link(); ?></p>
+					
 					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
 					<div>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 						<!-- .entry-content -->
 					
 						<?php endif; ?>
+					<div class="content_categoria">
+					<p>Categoría:</p>
+					<?php
+					$categories_list = get_the_category_list( __( ', ', 'twentythirteen' ) );
+					if ( $categories_list ) {
+						echo '<span class="categories-links">' . $categories_list . '</span>';
+					}
+					?>
 					</div>
+					</div>
+					
 					<div class="comentarios">
-						<div class="fb-comments" data-href="http://example.com/comments" data-width="600" data-colorscheme="light"></div>
+						<div class="fb-comments" data-href="http://127.0.0.1/CC/?p=84" data-width="700" data-colorscheme="light"></div>
 					</div>
 				</div>
 			</div>
